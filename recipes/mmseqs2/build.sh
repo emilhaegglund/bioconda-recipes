1725614e1cd0a03e68d558092dd25da2dce8ec3a
emilhaegglund <haegglund.emil@gmail.com>
2017-11-20 11:50:07 +0100

    added mmseqs2

---
+++ recipes/mmseqs2/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+if [[ $(uname) == "Darwin" ]]; then
+    echo "Configure for OSX"
+    CXX="$(brew --prefix)/bin/g++-7" cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=. ..
+else
+    echo "Configure for Linux"
+    cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=. ..
+fi
+
+make
+make install
+
